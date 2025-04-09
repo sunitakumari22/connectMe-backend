950dda908e1c3ee12d273669f340f213cb64580f
SunitaKumari <172397566+sunitakumari22@users.noreply.github.com>
2025-04-09 15:39:20 +0530

    updated the schema

--- joinedUser.js
+++ joinedUser.js
@@ -1,8 +1,7 @@
 const mongoose= require('mongoose');
  const joinedUserSchema=new mongoose.Schema({
-    name:String,
-    email:String,
-    mobile:String,
-    password:String,
+    name: String,
+    roomID: String,
+    interest: String
  });
 module.exports=mongoose.model('joinedUser',joinedUserSchema);
